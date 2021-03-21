1eebd59ea3d825e422cb50794c5bb85c060a4632
José Sancho <jsancho.dev@gmail.com>
2021-03-21 22:15:41 +0100

    Topic Cell ViewModel implemented

--- DiscourseClient/DiscourseClient/Topics/TopicCellViewModel.swift
+++ DiscourseClient/DiscourseClient/Topics/TopicCellViewModel.swift
@@ -6,15 +6,51 @@
 //  Copyright © 2020 Roberto Garrido. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol TopicCellViewModelDelegate: class {
+    func didDownload(image: UIImage?)
+}
 
 /// ViewModel que representa un topic en la lista
 class TopicCellViewModel {
     let topic: Topic
-    var textLabelText: String?
+    weak var delegate: TopicCellViewModelDelegate?
+    var imageStringURL: String?
+    var userImageURL: String?
+    var userImage: UIImage?
+    var titleText: String?
+    var postCountText: String?
+    var posterCountText: String?
+    var dateText: String?
     
     init(topic: Topic) {
         self.topic = topic
-        textLabelText = topic.title
+        titleText = topic.title
+        postCountText = "\(topic.postsCount)"
+        posterCountText = "\(topic.posters.count)"
+        dateText = topic.lastPostedAt
+        userImageURL = formatAvatarURL()
+        getPhotoImage(with: userImageURL)
+    }
+    
+    private func formatAvatarURL() -> String {
+        let apiURL = "https://mdiscourse.keepcoding.io"
+        guard let userURL = userImageURL else {
+            return apiURL
+        }
+        let formatedURL = userURL.replacingOccurrences(of: "{size}", with: "100")
+        return apiURL + formatedURL
+    }
+    
+    private func getPhotoImage(with imagePath: String?) {
+        guard let imageString = imagePath, let imageURL = URL(string: imageString) else { return }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            guard let imageData: Data = try? Data(contentsOf: imageURL) else { return }
+            let image: UIImage? = UIImage(data: imageData)
+            self.userImage = image
+            self.delegate?.didDownload(image: image)
+        }
     }
 }
